a89073fb429f8cbbeca5108a0229cc7495420896
moienkhan <moien.khan530@gmail.com>
2022-05-07 18:01:50 +0400

    djhfv;de

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,11 +3,11 @@
 
 @section('main-content')
 
-<section id="firstSection" class="page-header section-80vh bg-gradient-black ">
+<section id="firstSection" class="page-header section-80vh bg-gradient-black main-t"  style="background: url({{ asset('event/'.$record->image) }})">
     <div class="container">
-       <div class="row text-center">
-            <div class="col-xl-12 text-center">
-                <h1 class="blog-title text-white">{{$record->title}}</h1>
+       <div class="row ">
+            <div class="col-xl-12">
+                <h1 class="single-blog-title">{{$record->title}}</h1>
             </div>           
         </div>
     </div>
@@ -15,14 +15,14 @@
 
 <section class="page-main">
     <div class="section-small bg-light">
-        <div class="container" data-animation-container class="container" style="background: #fbf8f1; padding: 50px 30px;">
+        <div class="container" data-animation-container class="container" style="background: #fbf6f1; padding: 50px 30px;">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="blog-item bg-black2">
+                    <div class="blog-single-item bg-black2">
                         <div style="align-items: center">
-                            <div class=" lazy" data-bg="">
+                            <!-- <div class=" lazy" data-bg="">
                                 <img width="60%" height="80%" src="{{ asset('event/'.$record->image) }}">
-                            </div>
+                            </div> -->
                         </div>
                         <div class="blog-content">
                             {{-- <div class="blog-header">
@@ -32,6 +32,7 @@
                                     </p>
                                 </div>
                             </div> --}}
+                             
                             <p class="blog-text text-black-color">{!! $record->body !!}</p>
                         </div>
                     </div>
