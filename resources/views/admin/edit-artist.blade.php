8488351b070b6cb27a1cd20b0882c0fda090c62f
AliArthur <aarthurabel@gmail.com>
2022-05-09 14:45:44 +0400

    asdasdas

--- resources/views/admin/edit-artist.blade.php
+++ resources/views/admin/edit-artist.blade.php
@@ -119,13 +119,14 @@
                                             <div class="form-group row">
                                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Categories') }}</label>
                                                 <div class="col-md-6">
-                                                    <select class="class="form-control" name="categories[]" id='myselect' multiple="multiple">
-                                                    <option value="0">Select Category</option>
-                                                    @foreach($categories as $cat)
-                                                        @foreach($artist_categories as $cadt) 
-                                                            <option value="{{$cat->id}}"  {{ $cat->id == $cadt->id ? 'selected' : '' }}>{{$cat->name}}</option>
+                                                    {{-- {{dd($artist_categories)}} --}}
+                                                    
+                                                    <select class="form-control" name="categories[]" id='myselect' multiple="multiple">
+                                                        <option value="0" disabled>Select Category</option>
+                                                        @foreach($categories as $cat)
+                                                            <option value="{{$cat->id}}" {{ $artist_categories->contains($cat) ? 'selected' : '' }} >{{$cat->name}}</option>
+                                                            
                                                         @endforeach
-                                                    @endforeach
                                                     </select>
                                                 </div>
                                             </div>
